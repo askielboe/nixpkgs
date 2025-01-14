1984846399451c37542098c602ea5d94f488f088
r-vdp <ramses@well-founded.dev>
2025-01-14 16:10:08 +0100

    alacritty: add myself as maintainer, drop Mic92
    See https://github.com/NixOS/nixpkgs/pull/373582#discussion_r1914991708

--- pkgs/by-name/al/alacritty/package.nix
+++ pkgs/by-name/al/alacritty/package.nix
@@ -134,7 +134,7 @@ rustPlatform.buildRustPackage rec {
     mainProgram = "alacritty";
     maintainers = with maintainers; [
       Br1ght0ne
-      mic92
+      r-vdp
     ];
     platforms = platforms.unix;
     changelog = "https://github.com/alacritty/alacritty/blob/v${version}/CHANGELOG.md";
